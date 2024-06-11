4783c6a2fba3aae1032eec58b403bf05ff2a7e1c
Alexander Saul <alexandersaul2604@gmail.com>
2024-06-11 16:51:37 -0500

    Avance de servicio de categoria

--- src/main/java/com/alexandersaul/apiFinance/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/alexandersaul/apiFinance/services/impl/CategoryServiceImpl.java
@@ -1,13 +1,17 @@
 package com.alexandersaul.apiFinance.services.impl;
 
 import com.alexandersaul.apiFinance.models.Category;
+import com.alexandersaul.apiFinance.models.CategoryEntity;
 import com.alexandersaul.apiFinance.models.Transaction;
 import com.alexandersaul.apiFinance.repositories.CategoryRepository;
 import com.alexandersaul.apiFinance.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
@@ -19,7 +23,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAll() {
-        return List.of();
+        List<Category> categories = new ArrayList<>();
+        Iterable<CategoryEntity> categoryEntityIterable = categoryRepository.findAll();
+        //categoryEntityIterable.forEach((i) -> categories.add(t));
+        return null;
     }
 
     @Override
